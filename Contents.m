ca97c5bd76486e441c648a6c4a2ca835674af3e0
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-01-28 10:12:01 +0000

    First commit. Testing. Adding Contents.m

---
+++ Contents.m
@@ -0,0 +1,52 @@
+% MiningSuite
+% Version 0.6 (Sneak Peek) 28-January-2014
+%
+% SigMinr.
+%  Signal processing.
+%   sig.input       - Loads waveform from file.
+%   sig.rms         - Root Mean Square energy.
+%   sig.envelope    - Amplitude envelope.
+%   sig.spectrum    - FFT spectrum distribution.
+%   sig.autocor     - Autocorrelation function.
+%   sig.peak        - Peak picking.
+%
+%  Statistics.
+%   sig.hist        - Histogram.
+%
+%  Optional parameters.
+%   sig.ans         - Recalls the last design evaluation result.
+%   sig.chunklim    - Maximal chunk size allowed.
+%
+%
+% AudMinr
+%  Auditory modelling.
+%   aud.brightness  - Ratio of energy above a given cut-off frequency.
+%
+%
+% VocMinr
+%  Voice analysis.
+%   in preparation..
+%
+%
+% SeqMinr
+%  Sequence processing.
+%   Internal routines not available to end users.
+%
+%
+% PatMinr
+%  Pattern mining.
+%   Internal routines not yet available to end users.
+%
+%
+% MusMinr
+%  Music analysis.
+%   mus.minr        - Analysis in the symbolic domain.
+%   mus.tempo       - Tempo estimation.
+%
+% Much more operators are currently under preparation. The current
+% objectives are shown in poster.pdf, enclosed in the MiningSuite folder.
+
+% Copyright (C) 2014, Olivier Lartillot
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
