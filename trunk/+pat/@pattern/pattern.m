ebe7d879d536d7c80ea0d0ea85036b9d7e939528
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-01-29 00:51:33 +0000

    Adding copyright notices

--- trunk/+pat/@pattern/pattern.m
+++ trunk/+pat/@pattern/pattern.m
@@ -1,3 +1,7 @@
+% Copyright (C) 2014, Olivier Lartillot
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
 classdef pattern < hgsetget
 	properties (SetAccess = protected)
         parent
