f7fae877c733ef819dab8a2487d33f0a65353833
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-01-29 00:51:33 +0000

    Adding copyright notices

--- +pat/creatememory.m
+++ +pat/creatememory.m
@@ -1,3 +1,7 @@
+% Copyright (C) 2014, Olivier Lartillot
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
 function obj = creatememory(param,pattern)
     if isa(param,'seq.paramstruct') || isa(param,'pat.memostruct')
         obj = pat.memostruct(param,pattern);
--- +pat/verbose.m
+++ +pat/verbose.m
@@ -1,3 +1,7 @@
+% Copyright (C) 2014, Olivier Lartillot
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
 function v = verbose(b)
 
 persistent verbose
