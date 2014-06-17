9a2de7149305de2e367be052b1644f84ce65cddf
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-06-17 13:03:47 +0000

    sig.signal: 'Sstart' default set to 0

--- trunk/+sig/@signal/signal.m
+++ trunk/+sig/@signal/signal.m
@@ -7,6 +7,12 @@
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
+%
+% For any reuse of the code below, please mention the following
+% publication:
+% Olivier Lartillot, "The MiningSuite: ?MIRtoolbox 2.0? + ?MIDItoolbox 2.0?
+% + pattern mining + ...", AES 53RD INTERNATIONAL CONFERENCE, London, UK,
+% 2014
 
 classdef signal
 %%
@@ -241,7 +247,7 @@ function options = constructoptions
     
         sstart.key = 'Sstart';
         sstart.type = 'Numeric';
-        sstart.default = 1;
+        sstart.default = 0;
     options.sstart = sstart;
     
         srate.key = 'Srate';
