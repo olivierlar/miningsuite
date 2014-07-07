ff21b823a0f9e890f047d4c37fdec2f99d24b0aa
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-07-07 12:40:11 +0000

    aud.filterbank

---
+++ trunk/+sig/+filterbank/options.m
@@ -0,0 +1,37 @@
+% SIG.FILTERBANK.MAIN
+% Options related to sig.filterbank, also called by aud.filterbank
+%
+% Copyright (C) 2014, Olivier Lartillot
+%
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+
+function options = options
+    options = sig.signal.signaloptions();
+
+        nCh.key = 'NbChannels';
+        nCh.type = 'Integer';
+        nCh.default = Inf;
+    options.nCh = nCh;
+    
+        Ch.key = {'Channel','Channels'};
+        Ch.type = 'Integer';
+        Ch.default = 0;
+    options.Ch = Ch;
+    
+        freq.key = 'CutOff';
+        freq.type = 'Integer';
+        %freq.default = NaN;
+    options.freq = freq;
+    
+        overlap.key = 'Hop';
+        overlap.type = 'Boolean';
+        overlap.default = 1;
+    options.overlap = overlap;
+
+        filterorder.key = 'Order';
+        filterorder.type = 'Integer';
+        filterorder.default = 4;
+    options.filterorder = filterorder;
+end
---
+++ trunk/+sig/+filterbank/specif.m
@@ -0,0 +1,59 @@
+% SIG.FILTERBANK.SPECIF
+% Filterbank specification, called by sig.filterbank and aud.filterbank
+%
+% Copyright (C) 2014, Olivier Lartillot
+%
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+
+function [Hd,ch] = specif(option,sampling,nCh,ch)
+    %% Determination of the filterbank specifications
+    freqi = option.freq;
+    j = 1;
+    while j <= length(freqi)
+        if not(isinf(freqi(j))) && freqi(j)>sampling/2
+            if j == length(freqi)
+                freqi(j) = Inf;
+            else
+                freqi(j) = [];
+                j = j-1;
+            end
+        end
+        j = j+1;
+    end
+    step = option.overlap;
+    nchan = length(freqi)-step;
+    if nchan < 1
+        error('Error in sig.filterbank: the ''Cutoff'' parameter is not properly defined.');
+    end
+    for j = 1:nchan
+        if isinf(freqi(j))
+            [z{j},p{j},k{j}] = ellip(option.filterorder,3,40,...
+                                freqi(j+step)/sampling*2);
+        elseif isinf(freqi(j+step))
+            [z{j},p{j},k{j}] = ellip(option.filterorder,3,40,...
+                                freqi(j)/sampling*2,'high');
+        else
+            [z{j},p{j},k{j}] = ellip(option.filterorder,3,40,...
+                                freqi([j j+step])/sampling*2);
+        end
+    end
+    for j = 1:length(z)
+        [sos,g] = zp2sos(z{j},p{j},k{j});
+        Hd{j} = dfilt.df2tsos(sos,g);
+    end
+    ch = 1:length(freqi)-step;
+
+    for k = 1:length(Hd)
+        Hdk = Hd{k};
+        if ~iscell(Hdk)
+            Hdk = {Hdk};
+        end
+        for h = 1:length(Hdk)
+            if ~isa(Hdk{h},'function_handle')
+                Hdk{h}.PersistentMemory = true;
+            end
+        end
+    end
+end
