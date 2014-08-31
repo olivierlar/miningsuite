363a08f0538ea7de9d10af19fd71a76ca960a121
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-08-31 14:46:07 +0000

    Complete sig.autocor

--- trunk/+sig/@Autocor/Autocor.m
+++ trunk/+sig/@Autocor/Autocor.m
@@ -6,29 +6,86 @@
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
 
-classdef Autocor < sig.signal
+classdef Autocor < sig.Spectrum
+%%
     properties
         normalized = 0;
         window
     end
+%%
     methods
         function s = Autocor(varargin)
-          	s = s@sig.signal(varargin{:});
-            if strcmp(s.yname,'Signal')
+          	s = s@sig.Spectrum(varargin{:});
+            if strcmp(s.yname,'Spectrum')
                 s.yname = 'Autocor';
             end
             s.Xaxis.subunit = sig.subunit('Frequency','Hz',@time2freq);
             s.xname = 'Lag';
             s.xunit = 's';
         end
-        
+        %%
+        function obj = after(obj,option)
+            obj = after@sig.Spectrum(obj,option);
+            
+            if option.freq
+                obj.Xaxis.name = 'Frequency';
+            else
+                obj.Xaxis.name = 'Time';
+            end
+
+            if isstruct(option.min) || isstruct(option.max)
+                if ~isstruct(option.min)
+                    option.min.value = -Inf;
+                    option.min.unit = 's';
+                end
+                if ~isstruct(option.max)
+                    option.max.value = Inf;
+                    option.max.unit = 's';
+                end
+                param.value = [option.min.value,option.max.value];
+                param.unit = option.min.unit;
+                obj = obj.extract(param,'element','Xaxis',...
+                                        'Ydata','window');
+            end
+            
+            if not(isequal(option.normwin,0) || ...
+                   strcmpi(option.normwin,'No') || ...
+                   strcmpi(option.normwin,'Off') || ...
+                   obj.normalized)
+                obj = obj.normalize(option.normwin);
+            end
+            if option.hwr
+                obj = obj.hwr;
+            end
+            if max(option.enhance)>1
+                obj = obj.enhance(option.enhance);
+            end
+            
+            
+            function s = sindex(s,srate,sstart)
+                f = round((s-sstart)*srate) + 1;
+            end
+        end
+        %%
+        function obj = combinechunks(obj,new)
+            do = obj.Ydata;
+            dn = new.Ydata;
+            lo = do.size('element');
+            ln = dn.size('element');
+            if abs(lo-ln) <= 2  % Probleme of border fluctuation
+                mi = min(lo,ln);
+                do = do.extract('element',[1,mi]);
+                dn = dn.extract('element',[1,mi]);
+            elseif ln < lo
+                dn = dn.edit('element',lo,0);   % Zero-padding
+            elseif lo < ln
+                do = do.edit('element',ln,0);   % Zero-padding
+            end
+            obj.Ydata = do.plus(dn);
+        end
+        %%
         obj = normalize(obj,win);
         obj = hwr(obj);
         obj = enhance(obj,param);
     end
-end
-
-
-function f = time2freq(t)
-    f = 1./t;
 end
--- trunk/+sig/@Autocor/enhance.m
+++ trunk/+sig/@Autocor/enhance.m
@@ -1,83 +1,83 @@
-function obj = enhance(obj,e)
-    p = sig.peak(obj,'NoBegin','NoEnd','Contrast',.01,...
-                  'Normalize','Local');
-    v = sig.peak(obj,'Valleys','Contrast',.01,'Normalize','Local');
-    res = sig.compute(@main,obj.Ydata,p.peakpos,v.peakpos,e,obj.xdata);
+function obj = enhance(obj)
+    res = sig.compute(@main,obj.Ydata);
     obj.Ydata = res{1};
 end
 
 
-function out = main(d,p,v,e,t)
-    %if size(d,'frame') > 1
-    d = d.apply(@routine,{p,v,e,t},{'element'},1);
-    %end
+function out = main(d)
+    d = d.apply(@routine,{},{'frame'},Inf);
     out = {d};
 end
 
 
-function y = routine(x,p,v,e,t)
-    t = t(:);
+function y = routine(x)
+    if size(x,'frame')<=1
+        y = x;
+        return
+    end
+    pvk = pv{k}{l}{1,g,h};
     mv = [];
-    if not(isempty(p))
-        mp = min(p); %Lowest peak
-        mv = v(find(v<mp,1,'last'));
+    if not(isempty(pvk))
+        mp = min(pv{k}{l}{1,g,h}); %Lowest peak
+        vvv = vv{k}{l}{1,g,h}; %Valleys
+        mv = vvv(find(vvv<mp,1,'last'));
             %Highest valley below the lowest peak
 
         if not(isempty(mv))
-            x = x-mv;
+            cgh = cgh-mv;
         end
     end
-    y = x;
-    coef = x(2)-x(1); % initial slope of the autocor curve
-    tcoef = t(2)-t(1);
+    cgh2 = cgh;
+    tgh2 = t(:,g,1);
+    coef = cgh(2)-cgh(1); % initial slope of the autocor curve
+    tcoef = tgh2(2)-tgh2(1);
     deter = 0;
     inter = 0;
 
-    repet = find(not(diff(t)));  % Avoid bug if repeated x-values
+    repet = find(not(diff(tgh2)));  % Avoid bug if repeated x-values
     if repet
         warning('WARNING in MIRAUTOCOR: Two successive samples have exactly same temporal position.');
-        t(repet+1) = t(repet)+1e-12;
+        tgh2(repet+1) = tgh2(repet)+1e-12;
     end
 
     if coef < 0
         % initial descending slope removed
-        deter = find(diff(y)>0,1)-1;
+        deter = find(diff(cgh2)>0,1)-1;
             % number of removed points
         if isempty(deter)
             deter = 0;
         end
-        y(1:deter) = [];
-        t(1:deter) = [];
-        coef = y(2)-y(1);
+        cgh2(1:deter) = [];
+        tgh2(1:deter) = [];
+        coef = cgh2(2)-cgh2(1);
     end
 
     if coef > 0
         % initial ascending slope prolonged to the left
         % until it reaches the x-axis
-        while y(1) > 0
+        while cgh2(1) > 0
             coef = coef*1.1;
                 % the further to the left, ...
                 % the more ascending is the slope
                 % (not sure it always works, though...)
             inter = inter+1;
                 % number of added points
-            y = [y(1)-coef; y];
-            t = [t(1)-tcoef; t];
+            cgh2 = [cgh2(1)-coef; cgh2];
+            tgh2 = [tgh2(1)-tcoef; tgh2];
         end
-        y(1) = 0;
+        cgh2(1) = 0;
     end
-    
-    debug = 0;
-    for i = e  % Enhancing procedure
+
+    for i = option.e  % Enhancing procedure
         % option.e is the list of scaling factors
         % i is the scaling factor
         if i
-            be = find(t & t/i >= t(1),1);
+            be = find(tgh2 & tgh2/i >= tgh2(1),1);
                 % starting point of the substraction
                 % on the X-axis
 
             if not(isempty(be))
-                ic = interp1(t,y,t/i);
+                ic = interp1(tgh2,cgh2,tgh2/i);
                     % The scaled autocorrelation
                 ic(1:be-1) = 0;
                 ic(find(isnan(ic))) = Inf;
@@ -86,19 +86,19 @@ function y = routine(x,p,v,e,t)
                 ic = max(ic,0);
 
                 if debug
-                   hold off,plot(t,y)
+                   hold off,plot(tgh2,cgh2)
                 end
 
-                y = y - ic;       
+                cgh2 = cgh2 - ic;       
                     % The scaled autocorrelation
                     % is substracted to the initial one
 
-                y = max(y,0);
+                cgh2 = max(cgh2,0);
                     % Half-wave rectification
 
                 if debug
-                   hold on,plot(t,ic,'r')
-                   hold on,plot(t,y,'g')
+                   hold on,plot(tgh2,ic,'r')
+                   hold on,plot(tgh2,cgh2,'g')
                    drawnow
                    figure
                 end
@@ -109,13 +109,11 @@ function y = routine(x,p,v,e,t)
     % The  temporary modifications are
     % removed from the final curve
     if inter>=deter
-        y = y(inter-deter+1:end);
+        c(:,g,h) = cgh2(inter-deter+1:end);
         if not(isempty(mv))
-            y = y + mv;
+            c(:,g,h) = c(:,g,h) + mv;
         end
-    elseif size(y,1) > 1
-        y = [zeros(deter-inter,1);y];
-    elseif size(y,2) > 1
-        y = [zeros(1,deter-inter),y];
+    else
+        c(:,g,h) = [zeros(deter-inter,1);cgh2];
     end
 end
