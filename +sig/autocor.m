1337c1ca24b03e073299a7774c3d4aaf45d93548
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-09-28 19:30:54 +0000

    r311 actually introduced mus.autocor. Chunk decomposition now improved

--- +sig/autocor.m
+++ +sig/autocor.m
@@ -8,7 +8,8 @@
 
 function varargout = autocor(varargin)
     varargout = sig.operate('sig','autocor',sig.autocor.options,...
-                            @init,@main,varargin,@combinechunks);
+                            @init,@main,varargin,...
+                            @sig.autocor.combinechunks,'extensive');
 end
 
 
@@ -26,23 +27,4 @@ function out = main(x,option,postoption)
     else
         out = sig.autocor.after(x,postoption);
     end
-end
-
-
-function obj = combinechunks(obj,new)
-    do = obj.Ydata;
-    dn = new.Ydata;
-    lo = do.size('element');
-    ln = dn.size('element');
-    if lo == ln
-    elseif abs(lo-ln) <= 2  % Probleme of border fluctuation
-        mi = min(lo,ln);
-        do = do.extract('element',[1,mi]);
-        dn = dn.extract('element',[1,mi]);
-    elseif ln < lo
-        dn = dn.edit('element',lo,0);   % Zero-padding
-    elseif lo < ln
-        do = do.edit('element',ln,0);   % Zero-padding
-    end
-    obj.Ydata = do.plus(dn);
 end
