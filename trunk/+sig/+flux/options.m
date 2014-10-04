95401db315d9464afebf6b9ed94dde6ddad13eee
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-10-04 16:53:33 +0000

    sig.flux

---
+++ trunk/+sig/+flux/options.m
@@ -0,0 +1,45 @@
+function options = options    
+    options = sig.signal.signaloptions(.05,.5);
+
+        dist.key = 'Dist';
+        dist.type = 'String';
+        dist.default = 'Euclidean';
+    options.dist = dist;
+    
+        inc.key = 'Inc';
+        inc.type = 'Boolean';
+        inc.default = 0;
+    options.inc = inc;
+
+        bs.key = 'BackSmooth';
+        bs.type = 'String';
+        bs.choice = {'Goto','Lartillot'};
+        bs.default = '';
+        bs.keydefault = 'Goto';
+    options.bs = bs;
+    
+        complex.key = 'Complex';
+        complex.type = 'Boolean';
+        complex.default = 0;
+    options.complex = complex;
+    
+        gap.key = 'Gaps';
+        gap.type = 'Integer';
+        gap.default = 0;
+        gap.keydefault = .2;
+    options.gap = gap;
+        
+        h.key = 'Halfwave';
+        h.type = 'Boolean';
+        h.default = 0;
+        h.when = 'After';
+    options.h = h;
+    
+        median.key = 'Median';
+        median.type = 'Integer';
+        median.number = 2;
+        median.default = [0 0];
+        median.keydefault = [.2 1.3];
+        median.when = 'After';
+    options.median = median;
+end
