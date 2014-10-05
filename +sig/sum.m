d80f26638cb9deb1171db96293835f3ef3524a7c
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-10-05 14:28:37 +0000

    sig.sum

---
+++ +sig/sum.m
@@ -0,0 +1,54 @@
+% SIG.SUM
+% sums the channels together
+%
+% Copyright (C) 2014, Olivier Lartillot
+%
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+
+function varargout = sum(varargin)
+    varargout = sig.operate('sig','sum',options,...
+                            @init,@main,varargin,'concat');
+end
+
+
+function options = options
+    options = sig.signal.signaloptions();
+
+        mean.key = 'Mean';
+        mean.type = 'Boolean';
+        mean.default = 0;
+    options.mean = mean;
+end
+
+
+%%
+function [x type] = init(x,option,frame)
+    type = 'sig.signal';
+end
+
+
+function res = main(x,option,postoption)
+    if isempty(option)
+        res = x;
+        return
+    end
+    x = x{1};
+    if option.mean
+        norm = length(x.fbchannels);
+    else
+        norm = 1;
+    end
+    x.Ydata = sig.compute(@routine,x.Ydata,norm);
+    x.fbchannels = 1;
+    res = {x};
+end
+
+
+function out = routine(in,norm)
+    out = in.sum('fb_channel');
+    if norm ~= 1
+        out.content = out.content / norm;
+    end
+end
