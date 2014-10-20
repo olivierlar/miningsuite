cea02a2e633be6629250309d57dab9f1a81f8d83
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-10-20 18:51:37 +0000

    sig.segment(...,sig.peak)

--- trunk/+sig/segment.m
+++ trunk/+sig/segment.m
@@ -28,29 +28,56 @@ end
 
 function out = main(in,option,postoption)
     x = in{1};
-    s = {};
-    Sstart = x.Sstart;
-    si1 = 0;
-    for i = 1:length(option.pos)
-        si2 = find(x.sdata > option.pos(i),1);
-        if isempty(si2)
-            if si1
-                if si1 < x.Ssize
-                    s{end+1} = x.Ydata.content(si1:end);
-                end
-            else
-                s{end+1} = x.Ydata.content;
-            end
-            break
+    pos = option.pos;
+    if isa(pos,'sig.design')
+        pos = pos.eval;
+        if iscell(pos)
+            pos = pos{1};
         end
-        if si1
-            s{end+1} = x.Ydata.content(si1:si2-1);
-        elseif si2 > 1
-            s{end+1} = x.Ydata.content(1:si2-1);
-        end
-        si1 = si2;
-        Sstart(end+1) = option.pos(i);
     end
+    if isa(pos,'sig.signal')
+        pos = sort(pos.peakpos.content{1});
+    end
+    
+%     if 0 %strcmp(type,'sp')
+%         pos(pos > x.Ssize) = [];
+%         if pos(1) > 1
+%             pos = [1 pos];
+%         end
+%         %if pos(end) < x.Ydata.size('sample')
+%             pos(end+1) = x.Ydata.size('sample')+1;
+%         %end
+%         s = cell(1,length(pos)-1);
+%         Sstart = zeros(1,length(pos)-1);
+%         for i = 1:length(pos)-1
+%             s{i} = x.Ydata.content(pos(i):pos(i+1)-1);
+%             Sstart(i) = unit.generate(pos(i));
+%         end
+%    elseif strcmp(type,'s')
+        s = {};
+        Sstart = x.Sstart;
+        si1 = 0;
+        for i = 1:length(pos)
+            si2 = find(x.sdata > pos(i),1);
+            if isempty(si2)
+                if si1
+                    if si1 < x.Ssize
+                        s{end+1} = x.Ydata.content(si1:end);
+                    end
+                else
+                    s{end+1} = x.Ydata.content;
+                end
+                break
+            end
+            if si1
+                s{end+1} = x.Ydata.content(si1:si2-1);
+            elseif si2 > 1
+                s{end+1} = x.Ydata.content(1:si2-1);
+            end
+            si1 = si2;
+            Sstart(end+1) = pos(i);
+        end
+%    end
     x.Ydata.content = s;
     x.Sstart = Sstart;
     out = {x};
