eb66bf7a2002148abfb5424272bb8b7820b4194e
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-10-21 15:14:12 +0000

    sig.frame

---
+++ +sig/frame.m
@@ -0,0 +1,45 @@
+% SIG.FRAME
+%
+% Copyright (C) 2014, Olivier Lartillot
+%
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+
+function varargout = frame(varargin)
+    varargout = sig.operate('sig','frame',options,@init,@main,varargin);
+end
+
+
+function options = options
+        frame.key = 'Frame';
+        frame.type = 'Boolean';
+        frame.default = 0;
+        %frame.inner = strcmpi(inner,'inner');
+    options.frame = frame;
+    
+        fsize.key = 'FrameSize';
+        fsize.type = 'Unit';
+        fsize.default.unit = 's';
+        fsize.default.value = .05;
+        fsize.unit = {'s','sp'};
+    options.fsize = fsize;
+
+        fhop.key = 'FrameHop';
+        fhop.type = 'Unit';
+        fhop.default.unit = '/1';
+        fhop.default.value = .5;
+        fhop.unit = {'s','sp','Hz','/1'};
+    options.fhop = fhop;
+end
+
+
+function [x type] = init(x,option,frame)
+    x = sig.input(x,'FrameSize',frame.size.value,frame.size.unit,...
+                    'FrameHop',frame.hop.value,frame.hop.unit);
+    type = 'sig.signal';
+end
+
+
+function x = main(x,option,postoption)
+end
