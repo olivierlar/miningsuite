9d9d77573ed6723663b90711ac30d47f9d7b61b9
olartillot@gmail.com <olartillot@gmail.com@ae28a299-3b19-4cde-b0f0-830e092c1875>
2014-10-22 17:51:11 +0000

    sig.zerocross

---
+++ +sig/zerocross.m
@@ -0,0 +1,58 @@
+function varargout = zerocross(varargin)
+    varargout = sig.operate('sig','zerocross',...
+                            initoptions,@init,@main,varargin,'plus');
+end
+
+
+%%
+function options = initoptions
+    options = sig.signal.signaloptions(.05,.5);
+
+        per.key = 'Per';
+        per.type = 'String';
+        per.choice = {'Second','Sample'};
+        per.default = 'Second';
+    options.per = per;
+
+        dir.key = 'Dir';
+        dir.type = 'String';
+        dir.choice = {'One','Both'};
+        dir.default = 'One';
+    options.dir = dir;
+end
+
+
+%%
+function [x type] = init(x,option,frame)
+    type = 'sig.signal';
+end
+
+
+function out = main(in,option,postoption)
+    x = in{1};
+    if ~strcmpi(x.yname,'Brightness')
+        res = sig.compute(@routine,x.Ydata,x.Srate,option);
+        x = sig.signal(res,'Name','Zero-crossing rate',...
+                       'Deframe',in{1});
+    end
+    out = {x};
+end
+
+
+function out = routine(d,sr,option)
+    d = d.apply(@algo,{option,sr},{'sample'},3);
+    d = d.deframe;
+    out = {d};
+end
+
+
+function out = algo(x,option,sr)
+    n = sum(x(2:end,:,:).*x(1:end-1,:,:) < 0) / size(x,1);
+    if strcmp(option.per,'Second')
+        n = n * sr;
+    end
+    if strcmp(option.dir,'One')
+        n = n / 2;
+    end
+    out = n;
+end
