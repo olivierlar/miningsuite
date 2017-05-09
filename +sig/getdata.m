13c44221d51102d7b655c6a21b1ee8619fc07368
Olivier Lartillot <olartillot@gmail.com>
2017-05-09 21:48:42 +0200

    Sig.getdata

---
+++ +sig/getdata.m
@@ -0,0 +1,16 @@
+% SIG.GETDATA 
+%
+% Copyright (C) 2017, Olivier Lartillot
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+%
+% For any reuse of the code below, please mention the following
+% publication:
+% Olivier Lartillot, "The MiningSuite: ?MIRtoolbox 2.0? + ?MIDItoolbox 2.0?
+% + pattern mining + ...", AES 53RD INTERNATIONAL CONFERENCE, London, UK,
+% 2014
+
+function y = getdata(design)
+v = design.eval;
+y = v{1}.getdata;
