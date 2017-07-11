adaab72a43e6eead44f0832ad26b51cc9f9d60e7
Olivier Lartillot <olartillot@gmail.com>
2017-07-11 15:59:11 +0200

    Mus.tempo: 'Autocor' method transferred mostly to aud.tempo

--- +mus/tempo.m
+++ +mus/tempo.m
@@ -1,45 +1,57 @@
 % MUS.TEMPO 
+% estimates tempo
 %
-% Copyright (C) 2014, Olivier Lartillot
+% Copyright (C) 2017, Olivier Lartillot
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
 
 function varargout = tempo(varargin)
     varargout = sig.operate('mus','tempo',initoptions,@init,@main,...
-                            varargin,'plus');
+                            varargin,'plus','extensive');
 end
 
 
 %%
 function options = initoptions
-    options = sig.signal.signaloptions('FrameManual',3,.1);
+    options = aud.tempo.options;
     
         method.type = 'String';
         method.choice = {'Autocor','Pattern'};
         method.default = 'Autocor';
     options.method = method;
+    
+        reso.key = 'Resonance';
+        reso.type = 'String';
+        reso.choice = {'ToiviainenSnyder','vanNoorden',0,'off','no','New'};
+        reso.default = 'ToiviainenSnyder';
+    options.reso = reso;
 end
 
 
 %%
-function [x type] = init(x,option,frame)
+function [x, type] = init(x,option,frame)
     if strcmpi(option.method,'Autocor')
-        if ~x.istype('sig.autocor')
-            if frame.toggle
-                x = sig.envelope(x,'Halfwave','Diff',1,'Lambda',1,...
-                                    'FrameSize',frame.size.value,frame.size.unit,...
-                                   'FrameHop',frame.hop.value,frame.hop.unit);
-            else
-                x = sig.envelope(x,'Halfwave','Diff',1,'Lambda',1);
-            end
-            x = sig.autocor(x,'Max',5);
-        end
-        x = sig.peaks(x,'Total',1,'NoBegin');
+        [x, type] = aud.tempo.init(x,option,frame,@mus_autocor,@mus_spectrum);
     elseif strcmpi(option.method,'Pattern')
         x = mus.score(x,'Metre');
+        type = {'sig.signal','mus.Sequence'};
     end
-    type = {'sig.signal','sig.Autocor'};
+end
+
+
+function y = mus_autocor(x,option)
+    y = mus.autocor(x,'Min',60/option.ma,'Max',60/option.mi,...
+              'Enhanced',option.enh,...'NormalInput','coeff',...
+              'Resonance',option.reso,'NormalWindow',option.nw); %,...
+             % 'Phase',option.phase);
+end
+
+
+function y = mus_spectrum(x,option)
+    y = mus.spectrum(x,'Min',option.mi/60,'Max',option.ma/60,...
+                           'Prod',option.prod,...'NormalInput',...
+                           'Resonance',option.reso,'ZeroPad',option.zp);
 end
 
 
@@ -60,32 +72,6 @@ function out = main(in,option,postoption)
         t = sig.signal(d,'Name','Tempo','Srate',1); %'FbChannels',x.fbchannels??
         out = {t};
     else
-        x = in{1};
-        if strcmpi(x.yname,'Tempo')
-            out = in;
-        else
-            p = sig.compute(@routine,x.peakpos);
-            pc = zeros(1,length(p.content));
-            for i = 1:length(p.content)
-                if isempty(p.content(i))
-                    pc(i) = NaN;
-                else
-                    pc(i) = p.content(i);
-                end
-            end
-            t = sig.signal(pc','Name','Tempo','Srate',in{1}.Srate,'FbChannels',in{1}.fbchannels);
-            out = {t in{1}};
-        end
+        out = aud.tempo.main(in,option,postoption);
     end
-end
-
-
-function out = routine(d)
-    e = d.apply(@convert,{},{'element'});
-    out = {e};
-end
-
-
-function y = convert(x)
-    y = 60./x;
 end
