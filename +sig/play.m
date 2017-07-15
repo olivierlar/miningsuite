c32f4c6f3eef6941efd7bebe4d124c9524104f9d
Olivier Lartillot <olartillot@gmail.com>
2017-07-15 17:36:42 +0200

    Sig.play adapted to new convention

--- +sig/play.m
+++ +sig/play.m
@@ -8,86 +8,94 @@
 % the main folder of the MiningSuite distribution.
 function varargout = play(varargin)
 
-options = sig.signal.signaloptions();
+    options = sig.signal.signaloptions();
 
-    ch.key = 'Channel';
-    ch.type = 'Integer';
-    ch.default = 0;
-options.ch = ch;
+        ch.key = 'Channel';
+        ch.type = 'Integer';
+        ch.default = 0;
+    options.ch = ch;
 
-    sg.key = 'Segment';
-    sg.type = 'Integer';
-    sg.default = 0;
-options.sg = sg;
+        sg.key = 'Segment';
+        sg.type = 'Integer';
+        sg.default = 0;
+    options.sg = sg;
 
-    se.key = 'Sequence';
-    se.type = 'Integer';
-    se.default = 0;
-options.se = se;
+        se.key = 'Sequence';
+        se.type = 'Integer';
+        se.default = 0;
+    options.se = se;
 
-    inc.key = 'Increasing';
-    inc.type = 'MIRtb';
-options.inc = inc;
+        inc.key = 'Increasing';
+        inc.type = 'MIRtb';
+    options.inc = inc;
 
-    dec.key = 'Decreasing';
-    dec.type = 'MIRtb';
-options.dec = dec;
+        dec.key = 'Decreasing';
+        dec.type = 'MIRtb';
+    options.dec = dec;
 
-    every.key = 'Every';
-    every.type = 'Integer';
-options.every = every;
+        every.key = 'Every';
+        every.type = 'Integer';
+    options.every = every;
 
-    burst.key = 'Burst';
-    burst.type = 'Boolean';
-    burst.default = 1;
-options.burst = burst;
+        burst.key = 'Burst';
+        burst.type = 'Boolean';
+        burst.default = 1;
+    options.burst = burst;
 
 
-varargout = sig.operate('sig','play',options,@init,@main,varargin);
+    varargout = sig.operate('sig','play',options,@init,@main,@after,varargin);
+end
 
 
 function [x type] = init(x,option,frame)
-type = '?';
-
-
-function out = main(x,option,postoption)
-x = x{1};
-if iscell(x.design)
-    filenames = x.design;
-else
-    filenames = x.design.files;
+    type = '?';
 end
-sig.compute(@playfile,x.Ydata,filenames,x.Srate,x.sonifier);
-out = {};
+
+
+function out = main(x,option)
+    x = x{1};
+    if iscell(x.design)
+        filenames = x.design;
+    else
+        filenames = x.design.files;
+    end
+    sig.compute(@playfile,x.Ydata,filenames,x.Srate,x.sonifier);
+    out = {};
+end
 
 
 function varargout = playfile(d,name,rate,synth)
-if iscell(name)
-    name = name{1};
-end
-display(['Playing file: ' name]);
-nch = d.size('freqband');
-nfr = d.size('frame');
-for i = 1:nch
-    di = d;
-    if nch > 1
-        display(['Playing channel: ' num2str(i)]);
-        di = di.extract('freqband',i);
+    if iscell(name)
+        name = name{1};
     end
-    for j = 1:nfr
-        if nfr == 1
-            s = di.content;
-        else
-            s = di.view('frame',j);
+    display(['Playing file: ' name]);
+    nch = d.size('freqband');
+    nfr = d.size('frame');
+    for i = 1:nch
+        di = d;
+        if nch > 1
+            display(['Playing channel: ' num2str(i)]);
+            di = di.extract('freqband',i);
         end
-        tic
-        [s,rate] = synth(s,rate);
-        soundsc(s,rate);
-        idealtime = length(s)/rate;
-        practime = toc;
-        if practime < idealtime
-            pause(idealtime-practime)
+        for j = 1:nfr
+            if nfr == 1
+                s = di.content;
+            else
+                s = di.view('frame',j);
+            end
+            tic
+            [s,rate] = synth(s,rate);
+            soundsc(s,rate);
+            idealtime = length(s)/rate;
+            practime = toc;
+            if practime < idealtime
+                pause(idealtime-practime)
+            end
         end
     end
+    varargout = {1}; % How to avoid this?
 end
-varargout = {1}; % How to avoid this?
+
+
+function x = after(x,option)
+end
