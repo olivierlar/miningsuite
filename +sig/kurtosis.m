ccc5deff718d2e631a46eed91ce30572ac5e22f6
Olivier Lartillot <olartillot@gmail.com>
2017-07-18 19:43:46 +0200

    Sig.kurtosis

---
+++ +sig/kurtosis.m
@@ -0,0 +1,47 @@
+function varargout = kurtosis(varargin)
+    varargout = sig.operate('sig','kurtosis',...
+                            initoptions,@init,@main,@after,varargin);
+end
+
+
+%%
+function options = initoptions
+    options = sig.signal.signaloptions('FrameAuto',.05,.5);
+end
+
+
+%%
+function [x type] = init(x,option,frame)
+    if x.istype('sig.signal')
+        x = sig.spectrum(x,'FrameConfig',frame);
+    end
+    type = 'sig.signal';
+end
+
+
+function out = main(in,option)
+    x = in{1};
+    if ~strcmpi(x.yname,'Kurtosis')
+        res = sig.compute(@routine,x.Ydata,x.xdata);
+        x = sig.signal(res,'Name','Kurtosis',...
+                       'Srate',x.Srate,'Ssize',x.Ssize,...
+                       'FbChannels',x.fbchannels);
+    end
+    out = {x};
+end
+
+
+function out = routine(d,f)
+    e = d.apply(@algo,{f},{'element'},1);
+    out = {e};
+end
+
+
+function y = algo(d,f)
+    [s,c] = sig.spread.algo(d,f);
+    y = sum((f'-c).^4.*(d/sum(d))) ./ s.^4;
+end
+
+
+function x = after(x,option)
+end
