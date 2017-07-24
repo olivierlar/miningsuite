c4e4de5037864e0937336248d0255e8effe9f88c
Olivier Lartillot <olartillot@gmail.com>
2017-07-24 22:15:55 +0200

    Contents.m updated

--- Contents.m
+++ Contents.m
@@ -1,71 +1,84 @@
 % MiningSuite
-% Version 0.8.1 (Alpha version) 3-December-2014
+% Version 0.9 (Beta version) 3-December-2014
 %
-% It is still in development stage, so the results cannot be trusted for the moment..
-%
-% SigMinr.
-%  Signal processing
-%   sig.input           - Loads waveforms from files or Matlab arrays.
-%   sig.frame           - Decomposed into frames
-%   sig.rms             - Root Mean Square energy
-%   sig.envelope        - Amplitude envelope
-%   sig.spectrum        - FFT spectrum distribution
-%   sig.cepstrum        - Cepstrum
-%   sig.autocor         - Autocorrelation function
-%   sig.flux            - Distance between successive frames
-%   sig.filterbank      - Filterbank decomposition
-%   sig.peaks           - Peak picking
-%   sig.segment         - Segmentation
-%   sig.zerocross       - Zero-crossing rate
-%   sig.rolloff         - High-frequency energy
-%
-%  Statistics
-%   sig.hist            - Histogram
-%   sig.centroid        - Centroid
-%
-%   sig.ans             - Recalls the last design evaluation result.
-%   sig.chunklim        - Maximal chunk size allowed
+% It is still a Beta version, so the results cannot be trusted for the moment..
 %
 %
-% AudMinr
-%  Auditory modelling
-%   aud.envelope
-%   aud.spectrum
-%   aud.filterbank
-%   aud.brightness      - Brightness, as high-frequency energy
-%   aud.mfcc            - Mel-frequency cepstral coefficients
-%   aud.score           - Onset detection
-%   aud.eventdensity    - Temporal density of events
-%   aud.roughness       - Sensory dissonance
-%   aud.pitch           - F0 estimation
-%   aud.fluctuation     - Rhythmic periodicity
+% Sig.Minr: Signal processing package
+% 
+% sig.input: signal waveform
+% sig.frame: frame decomposition
+% sig.filterbank: filterbank decomposition
+% sig.envelope: envelope extraction
+% sig.spectrum: spectrum decomposition
+% sig.cepstrum: spectral analysis of spectrum
+% sig.autocor: autocorrelation function
+% sig.flux: contrast between successive frames
+% sig.sum: channel summation
+% sig.peaks: peak picking
+% sig.segment: waveform segmentation
+% sig.play: signal or data sonification
+% sig.length: temporal length
+% sig.rms: root mean square
+% sig.zerocross: waveform sign change rate
+% sig.rolloff: high-frequency ratio
+% sig.centroid: first statistical central moment
+% sig.spread: second statistical central moment
+% sig.skewness: third statistical central moment
+% sig.kurtosis: fourth statistical central moment
+% sig.flatness: distribution smoothness/pickiness
+% sig.entropy: Shannon entropy of distribution
+% sig.simatrix: self-similarity matrix
+% sig.novelty: transition between states
+% sig.mean: average along frames
+% sig.std: standard deviation along frames
+% sig.median: median along frames
+% sig.hist: histogram
 %
 %
-% MusMinr
-%  Music analysis
-%   mus.autocor
-%   mus.spectrum
-%   mus.tempo           - Tempo estimation
-%   mus.chromagram      - Energy distribution along pitches
-%   mus.keystrength     - Probability of key candidates
-%   mus.key             - Key estimation
-%   mus.score           - Analysis in the symbolic domain
-%   mus.pitch           - Pitch contour
+% Aud.Minr: Auditory modelling package
+% 
+% aud.filterbank: filterbank decomposition
+% aud.envelope: envelope extraction
+% aud.spectrum: spectrum decomposition
+% aud.flux: subband flux
+% aud.fluctuation: rhythmic periodicities along auditory channels
+% aud.events: event detection curve
+% aud.tempo: temporal periodicity of events
+% aud.pulseclarity: pulse clarity, beat strength
+% aud.brightness: high-frequency ratio
+% aud.mfcc: Mel-Frequency Cepstral Coefficients
+% aud.roughness: sensory dissonance
+% aud.pitch: stable fundamental frequencies
 %
 %
-% SeqMinr
-%  Sequence processing
+% Mus.Minr: Music analysis package
+% 
+% mus.spectrum: spectrum decomposition
+% mus.autocor: autocorrelation function
+% mus.tempo: tempo estimation
+% mus.pulseclarity: pulse clarity, beat strength
+% mus.pitch: notes pitch heights
+% mus.chromagram: energy distribution along pitches
+% mus.keystrength: key candidate scores
+% mus.key: tonal center positions and clarity
+% mus.majorness: major/minor mode clarity
+% mus.keysom: key self-organizing map projection
+% mus.tonalcentroid: projection along circles of fifths and thirds
+% mus.hcdf: Harmonic Change Detection Function
+%
+%
+% SeqMinr: Sequence processing
 %   Internal routines not available to end users
 %
 %
-% PatMinr
-%  Pattern mining
+% PatMinr: Pattern mining
 %   Internal routines not yet available to end users
 %
 %
 % More operators are currently under preparation.
 %
-% Copyright (C) 2014, Olivier Lartillot
+% Copyright (C) 2017, Olivier Lartillot
 % All rights reserved
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
