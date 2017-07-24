602795fa7dacb68a74c4d2fdbbc5857dfd3731a4
Olivier Lartillot <olartillot@gmail.com>
2017-07-24 22:12:12 +0200

    sig.median

---
+++ +sig/median.m
@@ -0,0 +1,49 @@
+function varargout = median(varargin)
+    varargout = sig.operate('sig','median',...
+                            initoptions,@init,@main,@after,varargin);
+end
+
+
+%%
+function options = initoptions
+    options = struct;
+end
+
+
+%%
+function [x type] = init(x,option,frame)
+    type = 'sig.signal';
+end
+
+
+function out = main(in,option)
+    x = in{1};
+    res = sig.compute(@routine,x.Ydata);
+    x = sig.signal(res,'Name','Median',...
+        'Xsampling',1,'Xstart',1,...
+        'Srate',x.Frate,'Ssize',x.Ssize);
+    out = {x};
+end
+
+
+function out = routine(d)
+    if find(strcmp('element',d.dims)) && d.size('element') > 1
+        dim = 'element';
+    else
+        dim = 'sample';
+    end
+    out = d.apply(@algo,{},{dim},1);
+    if strcmp(dim,'sample')
+        out = out.deframe;
+    end
+end
+
+
+function n = algo(x)
+    n = median(x);
+    n = n';
+end
+
+
+function x = after(x,option)
+end
