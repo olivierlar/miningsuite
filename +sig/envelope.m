cff47d4a5f792e369b20be4fffd5110fd5b43abc
Olivier Lartillot <olartillot@gmail.com>
2017-07-31 17:20:02 +0200

    sig.envelope(symbolic) deprecated

--- +sig/envelope.m
+++ +sig/envelope.m
@@ -3,10 +3,6 @@
 %
 % Copyright (C) 2014, 2017 Olivier Lartillot
 %
-% Code for envelope generation from MIDI file is taken from onsetacorr.m
-%   and duraccent.m, part of MIDI Toolbox. Copyright (C) 2004, University of 
-%   Jyvaskyla, Finland
-%
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
@@ -14,7 +10,7 @@
 function varargout = envelope(varargin)
     varargout = sig.operate('sig','envelope',sig.envelope.options,...
                             @sig.envelope.init,...
-                            {@sig.envelope.main,@symbolic},@after,...
+                            @sig.envelope.main,@after,...
                             varargin,'concat','extensive');
 end
 
@@ -45,27 +41,4 @@ function out = after(x,option)
     x.Ydata = sig.envelope.diff(x,option);
     x = sig.envelope.after(x,option);
     out = {x};
-end
-
-
-function out = symbolic(x,option)
-    % Code adapted from onsetacorr.m and duraccent.m in MIDItoolbox
-    % NDIVS = divisions in Hz (default = 4);
-    ndivs = 4;
-    ob = x(:,6); % onsets in sec.
-    
-    dur = x(:,7); % durations in sec.
-    tau=0.5;
-    accent_index=2;
-    d = (1-exp(-dur/tau)).^accent_index; % duration accent by Parncutt (1994)
-    
-    vlen = ndivs*(ceil(ob(end))+1);
-    g = zeros(vlen,1);
-    ind = mod(round(ob*ndivs),vlen)+1;
-    for k=1:length(ind)
-        g(ind(k)) = g(ind(k))+d(k);
-    end
-    %%
-    d = sig.data(g,{'sample'});
-    out = {sig.Envelope(d,'Srate',ndivs,'Sstart',0,'Ssize',length(g))};
 end
