0606f7e731ee745241ff86c055a5af61d749bb0d
Olivier Lartillot <olartillot@gmail.com>
2017-09-17 19:35:21 +0200

    sig.input renamed sig.signal

--- +sig/signal.m
+++ +sig/signal.m
@@ -1,26 +1,26 @@
-% SIG.INPUT simply loads the waveform stored in a soundfile and performs
+% SIG.SIGNAL simply loads the waveform stored in a soundfile and performs
 % basic post-processing.
-%   SIG.INPUT('filename') loads the waveform stored in the file named 
+%   SIG.SIGNAL('filename') loads the waveform stored in the file named 
 %       'filename'. The data is stored into a sig.Signal object.
-%   SIG.INPUT('Folder') loads all the sound files in the CURRENT folder
+%   SIG.SIGNAL('Folder') loads all the sound files in the CURRENT folder
 %       into a single sig.Signal object.
 %   Transformation options:
-%       SIG.INPUT(...,'Mix','No') does not perform the default summing of
+%       SIG.SIGNAL(...,'Mix','No') does not perform the default summing of
 %           channels into one single mono track, but instead stores each 
 %           channel of the initial soundfile separately. 
-%       SIG.INPUT(...,'Center') centers the waveform(s).
-%       SIG.INPUT(...,'Sampling',r) resamples at rate r (in Hz).
-%       SIG.INPUT(...,'Extract',t1,t2,u) extracts the signal between dates
+%       SIG.SIGNAL(...,'Center') centers the waveform(s).
+%       SIG.SIGNAL(...,'Sampling',r) resamples at rate r (in Hz).
+%       SIG.SIGNAL(...,'Extract',t1,t2,u) extracts the signal between dates
 %           t1 and t2, expressed in the unit u.
 %           Possible values for u:
 %               's' (seconds, by default),
 %               'sp' (sample index, starting from 1).
-%       SIG.INPUT(...,...,'Trim') trims the pseudo-silence beginning and
+%       SIG.SIGNAL(...,...,'Trim') trims the pseudo-silence beginning and
 %           end off the audio file. Silent frames are frames with RMS below 
 %           t times the medium RMS of the whole audio file.
 %           Default value: t = 0.06
-%       SIG.INPUT(...,'TrimThreshold',t) specifies the trimming threshold t.
-%   SIG.INPUT(...,'Frame',...) decomposes the waveform into successive
+%       SIG.SIGNAL(...,'TrimThreshold',t) specifies the trimming threshold t.
+%   SIG.SIGNAL(...,'Frame',...) decomposes the waveform into successive
 %       frames. However we advise to use 'Frame' directly with the
 %       operators for which frame decomposition is actually used.
 %
@@ -31,8 +31,8 @@
 %
 % See also SIG.SIGNAL
 
-function varargout = input(varargin)
-varargout = sig.operate('sig','input',...
+function varargout = signal(varargin)
+varargout = sig.operate('sig','signal',...
                         sig.Signal.signaloptions('FrameAuto',.05,.5),...
                         sig.Signal.initmethod,sig.Signal.mainmethod,...
                         sig.Signal.aftermethod,varargin,'concat_sample');
