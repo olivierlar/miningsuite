659f49f07754f2506eb9e8b4e22e2918c55ec8ba
Olivier Lartillot <olartillot@gmail.com>
2017-12-30 18:44:26 +0100

    sig.envelope 'Spectro' 'Complex' 'Diff' unavailable (also not working currently in MIRtoolbox)

--- +sig/+envelope/diff.m
+++ +sig/+envelope/diff.m
@@ -16,14 +16,7 @@ end
 
 function d = main(d,postoption)
     if postoption.complex
-        dph = diff(ph{k}{i},2);
-        dph = dph/(2*pi);% - round(dph/(2*pi));
-        ddki = sqrt(d{k}{i}(3:end,:,:).^2 + d{k}{i}(2:end-1,:,:).^2 ...
-                                  - 2.*d{k}{i}(3:end,:,:)...
-                                     .*d{k}{i}(2:end-1,:,:)...
-                                     .*cos(dph));
-        d{k}{i} = d{k}{i}(2:end,:,:); 
-        tp{k}{i} = tp{k}{i}(2:end,:,:);
+        error('Option not available yet.')
     else
         d = d.apply(@zdiff,{},{'sample'});
     end
