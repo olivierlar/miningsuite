a1b3b0da60f96a73e41afe79bae8f74c596899ae
Olivier Lartillot <olartillot@gmail.com>
2018-01-02 14:49:07 +0100

    sig.subunit: new field 'parameter'

--- +sig/@subunit/subunit.m
+++ +sig/@subunit/subunit.m
@@ -1,6 +1,6 @@
 % SIG.SUBUNIT CLASS
 %
-% Copyright (C) 2014 Olivier Lartillot
+% Copyright (C) 2014, 2018 Olivier Lartillot
 %
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
@@ -12,13 +12,17 @@ classdef subunit
         dimname = ''
         unitname = ''
         converter
+        parameter = NaN
     end
 %%
     methods
-        function obj = subunit(dimname,unitname,converter)
+        function obj = subunit(dimname,unitname,converter,parameter)
             obj.dimname = dimname;
             obj.unitname = unitname;
             obj.converter = converter;
+            if nargin > 3
+                obj.parameter = parameter;
+            end
         end
     end
 end
