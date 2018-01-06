da827b5bf2efafeb49f0bf6fb4186e2a5a2efac3
Olivier Lartillot <olartillot@gmail.com>
2018-01-06 12:19:57 +0100

    sig.Cepstrum copyright 2018

--- +sig/@Cepstrum/Cepstrum.m
+++ +sig/@Cepstrum/Cepstrum.m
@@ -1,6 +1,6 @@
 % sig.Cepstrum class
 %
-% Copyright (C) 2014, 2017 Olivier Lartillot
+% Copyright (C) 2014, 2017-2018 Olivier Lartillot
 %
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
