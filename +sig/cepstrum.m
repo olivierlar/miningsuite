3bea3b90eaa2eb99c84855b6c1cd17067bcc1a83
Olivier Lartillot <olartillot@gmail.com>
2018-01-06 12:18:58 +0100

    sig.cepstrum copyright 2018

--- +sig/cepstrum.m
+++ +sig/cepstrum.m
@@ -2,7 +2,7 @@
 % computes the cepstrum, which indicates periodicities, and is used for 
 % instance for pitch detection
 %
-% Copyright (C) 2014, 2017 Olivier Lartillot
+% Copyright (C) 2014, 2017-2018 Olivier Lartillot
 %
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
