865a8c1d3c8fd8f6d22a0fb20263a7c1ee2f7485
Olivier Lartillot <olartillot@gmail.com>
2018-01-13 18:11:51 +0100

    Copyright

--- +sig/@Signal/resample.m
+++ +sig/@Signal/resample.m
@@ -1,3 +1,11 @@
+% SIG.SIGNAL.RESAMPLE
+%
+% Copyright (C) 2014, 2017-2018 Olivier Lartillot
+%
+% All rights reserved.
+% License: New BSD License. See full text of the license in LICENSE.txt in
+% the main folder of the MiningSuite distribution.
+
 function obj = resample(obj,newrate)
     [obj.Ydata,obj.Sstart] = sig.compute(@main,obj.Ydata,obj.Srate,newrate,obj.Sstart);
     obj.Srate = repmat(newrate,size(obj.Srate));
