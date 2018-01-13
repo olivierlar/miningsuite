21a4fe960af772479955dfa3acfac97d664da091
Olivier Lartillot <olartillot@gmail.com>
2018-01-13 18:07:36 +0100

    sig.Signal.resample adapted to new Sstart convention from commit 00102bbc5858d8990b2c9a6250d6aca7f87b19dc

--- +sig/@Signal/resample.m
+++ +sig/@Signal/resample.m
@@ -1,12 +1,13 @@
 function obj = resample(obj,newrate)
-    obj.Ydata = sig.compute(@main,obj.Ydata,obj.Srate,newrate);
+    [obj.Ydata,obj.Sstart] = sig.compute(@main,obj.Ydata,obj.Srate,newrate,obj.Sstart);
     obj.Srate = repmat(newrate,size(obj.Srate));
 end
     
    
-function out = main(data,oldrate,newrate)
+function out = main(data,oldrate,newrate,Sstart)
     if ~(oldrate == newrate)
         data = data.apply(@resample,{newrate,oldrate},{'sample'},2);
-    end    
-    out = {data};
+    end
+    Sstart = Sstart * newrate / oldrate;
+    out = {data,Sstart};
 end
