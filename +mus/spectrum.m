5c215188cf1fb86bfd42b843c68d55d8d9cd3652
Olivier Lartillot <olartillot@gmail.com>
2018-01-14 20:30:05 +0100

    mus.spectrum(sig.segment,'Cents')

--- +mus/spectrum.m
+++ +mus/spectrum.m
@@ -54,38 +54,12 @@ function out = after(x,option)
     tmp = res{2};
     
     if strcmp(x.xname,'Frequency') && (option.cent || option.collapsed)
-        f = x.xdata;
-        isgood = f*(2^(1/1200)-1) >= f(2)-f(1);
-        good = find(isgood);
-        if isempty(good)
-            error('ERROR in MUS.SPECTRUM: The frequency resolution of the spectrum is too low to be decomposed into cents.');
-        end
-        if good>1
-            warning(['MIRSPECTRUM: Frequency resolution in cent is achieved for frequencies over ',...
-                num2str(floor(f(good(1)))),' Hz. Lower frequencies are ignored.'])
-            display('Hint: if you specify a minimum value for the frequency range (''Min'' option)');
-            display('      and if you do not specify any frequency resolution (''Res'' option), ');
-            display('      the correct frequency resolution will be automatically chosen.');
-        end
-        f = f(good);
-        x.Ydata = x.Ydata.extract('element',[good(1),good(end)]);
-        f2cents = 440*2.^(1/1200*(-1200*10:1200*10-1)');
-            % The frequencies corresponding to the cent range
-        cents = repmat((0:1199)',[20,1]);
-            % The cent range is for the moment centered to A440
-        octaves = ones(1200,1)*(-10:10);
-        select = find(f2cents>f(1) & f2cents<f(end));
-            % Cent range actually used in the current spectrum
-        f2cents = f2cents(select);
-        cents = cents(select);
-        octaves = octaves(select);
-        x.Ydata = x.Ydata.apply(@interp,{f,f2cents},{'element'});
+        [x.Ydata,origin] = sig.compute(@cents,x.Ydata,x.xdata);
         x.Xaxis.name = 'Pitch';
         x.Xaxis.unit.name = 'cents';
         x.Xaxis.start = 0;
-        x.Xaxis.unit.origin = octaves(1)*1200 + cents(1) + 6900;
+        x.Xaxis.unit.origin = origin;
         x.Xaxis.unit.rate = 1;
-        % Now the cent range is expressed in midicent
         x.yname = 'cent-Spectrum';
         x.phase = [];
     end
@@ -100,6 +74,37 @@ function out = after(x,option)
     end
         
     out = {x,tmp};
+end
+
+
+function out = cents(d,f)
+    isgood = f*(2^(1/1200)-1) >= f(2)-f(1);
+    good = find(isgood);
+    if isempty(good)
+        error('ERROR in MUS.SPECTRUM: The frequency resolution of the spectrum is too low to be decomposed into cents.');
+    end
+    if good>1
+        warning(['MIRSPECTRUM: Frequency resolution in cent is achieved for frequencies over ',...
+            num2str(floor(f(good(1)))),' Hz. Lower frequencies are ignored.'])
+        display('Hint: if you specify a minimum value for the frequency range (''Min'' option)');
+        display('      and if you do not specify any frequency resolution (''Res'' option), ');
+        display('      the correct frequency resolution will be automatically chosen.');
+    end
+    f = f(good);
+    d = d.extract('element',[good(1),good(end)]);
+    f2cents = 440*2.^(1/1200*(-1200*10:1200*10-1)');
+        % The frequencies corresponding to the cent range
+    cents = repmat((0:1199)',[20,1]);
+        % The cent range is for the moment centered to A440
+    octaves = ones(1200,1)*(-10:10);
+    select = find(f2cents>f(1) & f2cents<f(end));
+        % Cent range actually used in the current spectrum
+    f2cents = f2cents(select);
+    cents = cents(select);
+    octaves = octaves(select);
+    d = d.apply(@interp,{f,f2cents},{'element'});
+    origin = octaves(1)*1200 + cents(1) + 6900;
+    out = {d,origin};
 end
 
 
