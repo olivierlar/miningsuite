e2d0c885788bbca9b249549c182545e1bffdb29c
Olivier Lartillot <olartillot@gmail.com>
2018-01-16 20:44:05 +0100

    sig.save not available yet

--- +sig/save.m
+++ +sig/save.m
@@ -1,12 +1,21 @@
-% Copyright (C) 2017 Olivier Lartillot
+% SIG.SAVE
+% (Not available yet.)
+%
+% Copyright (C) 2017-2018 Olivier Lartillot
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
 
 function varargout = save(varargin)
+arg = varargin{1};
 
-if isa(varargin{1},'sig.Signal')
-    varargout = varargin{1}.save(varargin{2:end});
+if isa(arg,'sig.design')
+    e = arg.eval;
+    arg = e{1};
+end
+
+if isa(arg,'sig.Signal')
+    varargout = arg.save(varargin{2:end});
     return
 end
 
