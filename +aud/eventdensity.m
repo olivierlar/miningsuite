c712395f89e545e9d8e5480e9f40ca9e070b0fbe
Olivier Lartillot <olartillot@gmail.com>
2018-01-24 21:24:51 +0100

    aud.eventdensity adapted to architecture changes (892bf2e1d121c3a974457dc06b419f3fd0e450b2 and 564355f14f49dfa503ab732d64958654ed7df1cb)

--- +aud/eventdensity.m
+++ +aud/eventdensity.m
@@ -1,46 +1,29 @@
 % AUD.EVENTDENSITY 
 %
-% Copyright (C) 2014, 2017 Olivier Lartillot
+% Copyright (C) 2014, 2017-2018 Olivier Lartillot
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
 
 function varargout = eventdensity(varargin)
     varargout = sig.operate('aud','eventdensity',initoptions,@init,@main,...
-                            varargin);
+                            @after,varargin);
 end
 
 
 %%
 function options = initoptions
-    options = sig.Signal.signaloptions('FrameManual');
-
-        frame.key = 'Frame';
-        frame.type = 'Boolean';
-        frame.default = 0;
-    options.frame = frame;
-    
-        fsize.key = 'FrameSize';
-        fsize.type = 'Unit';
-        fsize.default.unit = 's';
-        fsize.default.value = 5;
-        fsize.unit = {'s'};
-    options.fsize = fsize;
-
-        fhop.key = 'FrameHop';
-        fhop.type = 'Unit';
-        fhop.default.unit = '/1';
-        fhop.default.value = .2;
-        fhop.unit = {'s','Hz','/1'};
-    options.fhop = fhop;
+    options = sig.Signal.signaloptions('FrameManual',5,.2,'After');
 end
 
 
 %%
-function [x type] = init(x,option,frame)
+function [x type] = init(x,option)
     if isa(x,'sig.design')
         x = x.eval;
-        x = x{1};
+        if iscell(x)
+            x = x{1};
+        end
     end
     if ~isa(x,'aud.Sequence')
         x = aud.score(x);
@@ -49,18 +32,36 @@ function [x type] = init(x,option,frame)
 end
 
 
-function out = main(in,option,frame)
-    fsize = frame.size.value;
-    fhop = frame.hop.value; % Check unit..
-    fhop = fsize * fhop;
-    
+function out = main(in,option)
     if isempty(in.content)
         d = sig.data([],{'sample'});
     else
         t1 = in.content{1}.parameter.getfield('onset').value;
         tend = in.content{end}.parameter.getfield('onset').value;
-        nbframes = ceil((tend - t1) / fhop);
-
+        if option.frame
+            if strcmpi(option.fsize.unit,'s')
+                fsize = option.fsize.value;
+            elseif strcmpi(option.fsize.unit,'sp')
+                error('Error: ''sp'' not adequate for symbolic data');
+            end
+            if strcmpi(option.fhop.unit,'/1')
+                fhop = option.fhop.value*fsize;
+            elseif strcmpi(option.fhop.unit,'%')
+                fhop = option.fhop.value*fsize*.01;
+            elseif strcmpi(option.fhop.unit,'s')
+                fhop = option.fhop.value;
+            elseif strcmpi(option.fhop.unit,'sp')
+                error('Error: ''sp'' not adequate for symbolic data');
+            elseif strcmpi(option.fhop.unit,'Hz')
+                fhop = 1/option.fhop.value;
+            end
+            nbframes = max(1,floor((tend-fsize)/fhop)+1); % Number of frames
+        else
+            nbframes = 1;
+            fsize = tend-t1;
+            fhop = 0;
+        end
+        
         t = zeros(1,nbframes);
         ed = zeros(1,nbframes);
         i1 = 1;
@@ -73,7 +74,7 @@ function out = main(in,option,frame)
                     break
                 end
             end
-            ed(i) = i2 - i1;
+            ed(i) = (i2 - i1) / fsize;
             while in.content{i1}.parameter.getfield('onset').value < t(i)+fhop
                 i1 = i1+1;
                 if i1 > length(in.content)
@@ -85,4 +86,8 @@ function out = main(in,option,frame)
     end
     ed = sig.Signal(d,'Name','Event density','Srate',1/fhop); %'FbChannels',x.fbchannels??
     out = {ed};
+end
+
+
+function x = after(x,option)
 end
