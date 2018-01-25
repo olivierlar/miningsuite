05dae338fb7a93428f2fe71b70b5fc72a650df1d
Olivier Lartillot <olartillot@gmail.com>
2018-01-25 17:05:59 +0100

    aud.pulseclarity: only MaxAutocor implemented for the moment

--- +aud/+pulseclarity/main.m
+++ +aud/+pulseclarity/main.m
@@ -1,6 +1,6 @@
 % AUD.PULSECLARITY.MAIN
 %
-% Copyright (C) 2017 Olivier Lartillot
+% Copyright (C) 2017-2018 Olivier Lartillot
 %
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
@@ -20,5 +20,7 @@ function out = main(in,option)
         d = sig.compute(@max,x.Ydata);
         pc = sig.Signal(d,'Name','Pulse Clarity','Srate',in{1}.Srate,'FbChannels',in{1}.fbchannels);
         out = {pc x};
+    else
+        error('ERROR IN AUD.PULSECLARITY: Sorry, heuristics not implemented yet.');
     end
 end
