0e6b09faa5045f141db0f90750fa55dd30de9e0e
Olivier Lartillot <olartillot@gmail.com>
2018-02-03 14:07:04 +0100

    sig.kurtosis was not storing Sstart Send from input

--- +sig/kurtosis.m
+++ +sig/kurtosis.m
@@ -24,7 +24,7 @@ function [x type] = init(x,option,frame)
     if x.istype('sig.Signal')
         if option.frame
             x = sig.frame(x,'FrameSize',option.fsize.value,option.fsize.unit,...
-                'FrameHop',option.fhop.value,option.fhop.unit);
+                          'FrameHop',option.fhop.value,option.fhop.unit);
         end
         x = sig.spectrum(x);   
     end
@@ -37,8 +37,8 @@ function out = main(in,option)
     if ~strcmpi(x.yname,'Kurtosis')
         res = sig.compute(@routine,x.Ydata,x.xdata);
         x = sig.Signal(res,'Name','Kurtosis',...
-                       'Srate',x.Srate,'Ssize',x.Ssize,...
-                       'FbChannels',x.fbchannels);
+                       'Srate',x.Srate,'Sstart',x.Sstart,'Send',x.Send,...
+                       'Ssize',x.Ssize,'FbChannels',x.fbchannels);
     end
     out = {x};
 end
