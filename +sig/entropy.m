acbad4e07391cb3d6b9f70eb1036da30236cb2e0
Olivier Lartillot <olartillot@gmail.com>
2018-02-03 14:19:01 +0100

    sig.entropy was not storing Sstart Send from input

--- +sig/entropy.m
+++ +sig/entropy.m
@@ -1,7 +1,7 @@
 % SIG.ENTROPY
 %
 % Copyright (C) 2017-2018 Olivier Lartillot
-% ? 2007-2009 Olivier Lartillot & University of Jyvaskyla
+% Copyright (C) 2007-2009 Olivier Lartillot & University of Jyvaskyla
 %
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
@@ -21,21 +21,15 @@ function options = initoptions
         center.type = 'Boolean';
         center.default = 0;
     options.center = center;
-    
-        minrms.key = 'MinRMS';
-        minrms.when = 'After';
-        minrms.type = 'Numerical';
-        minrms.default = .005;
-    options.minrms = minrms;
 end
 
 
 %%
-function [x type] = init(x,option,frame)
+function [x,type] = init(x,option)
     if x.istype('sig.Signal')
         if option.frame
             x = sig.frame(x,'FrameSize',option.fsize.value,option.fsize.unit,...
-                'FrameHop',option.fhop.value,option.fhop.unit);
+                          'FrameHop',option.fhop.value,option.fhop.unit);
         end
         x = sig.spectrum(x);   
     end
@@ -48,8 +42,8 @@ function out = main(in,option)
     if ~strcmpi(x.yname,'Entropy')
         res = sig.compute(@routine,x.Ydata,option);
         x = sig.Signal(res,'Name','Entropy',...
-                       'Srate',x.Srate,'Ssize',x.Ssize,...
-                       'FbChannels',x.fbchannels);
+                       'Srate',x.Srate,'Sstart',x.Sstart,'Send',x.Send,...
+                       'Ssize',x.Ssize,'FbChannels',x.fbchannels);
     end
     out = {x};
 end
