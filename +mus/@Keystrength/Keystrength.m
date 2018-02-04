f6823aa8ba75f5c15f0e3c18be27eda64f7b2eff
Olivier Lartillot <olartillot@gmail.com>
2018-02-04 21:40:50 +0100

    mus.Keystrength: xunsampled, get

--- +mus/@Keystrength/Keystrength.m
+++ +mus/@Keystrength/Keystrength.m
@@ -20,6 +20,17 @@ classdef Keystrength < sig.Signal
             end
             s.Xaxis.name = 'Keys';    
             s.xsampling = 1;
+            s.xunsampled = {'C','C#','D','D#','E','F','F#','G','G#','A','A#','B','c','c#','d','d#','e','f','f#','g','g#','a','a#','b'};
+        end
+    
+        function d = get(obj,field)
+            if strcmpi(field,'strength')
+                d = obj.Ydata;
+            elseif strcmpi(field,'key')
+                d = obj.xdata;
+            else
+                d = get@sig.Signal(obj,field);
+            end
         end
     end
 end
