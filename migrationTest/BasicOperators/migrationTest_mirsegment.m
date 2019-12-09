5b35ead3ab2046ac43bbc9860d43852e454b40b9
Olivier Lartillot <olartillot@gmail.com>
2019-12-09 17:44:31 +0100

    migrationTest_mirsegment

--- migrationTest/BasicOperators/migrationTest_mirsegment.m
+++ migrationTest/BasicOperators/migrationTest_mirsegment.m
@@ -9,14 +9,17 @@ disp('<strong>testing migration: miregment(...) -> sig.segment(...)</strong>');
 clearvars -except testfile ;
 
 a = miraudio(testfile);
+e = mirenvelope(a);
+p = mirpeaks(e);
+s1 = mirsegment(a,p);
+
 b = sig.signal(testfile,'Mix');
+e = sig.envelope(b);
+p = sig.peaks(e);
+s2 = sig.segment(b,p);
 
-a = mirsegment(a);
-b = sig.segment(b);
-
-x = mirgetdata(a);
-%x = get(a, 'Data');
-y = b.getdata;
+x = get(s1, 'Data');
+y = s2.getdata;
 
 tf = isequal(x,y);
 
@@ -34,51 +37,11 @@ else
 end
 
 return
-%% testing mirsegment: mirsegment(... , p) -> sig.segment(... , p)
-clearvars -except testfile ;
-
-disp('<strong>testing migration: miregment(..., p) -> sig.segment(..., p)</strong>'); 
-a = miraudio(testfile);
-b = sig.signal(testfile,'Mix');
-
-p1 = mirpeaks(testfile);
-p2 = sig.peaks(testfile, 'Mix');
-
-a = mirsegment(a,p1);
-b = sig.segment(b, p2);
-
-x = mirgetdata(a);
-y = b.getdata;
-
-tf = isequal(x,y);
-
-x = (mirgetdata(a));
-y = squeeze(b.getdata);
 
 
-col = (size(x));
-col = col(1);
-row = size(x);
-row = row(2);
-z = [];
-for i = 1:row
-    firstNaN = find(isnan( (x(:,i) )));
+%%
+% using mirpitch
 
-    if(isempty(firstNaN) == 1 )
-       z{i} = x(1:col,i);
-    else
-        size = firstNaN(1)-1;
-        z{i} = x(1:size-1,i);
-    end
-
-end
-
-if tf == 1
-   cprintf('*green', 'test SUCCESS!\n'); 
-
-else
-   debugFail(x,y);
-end
 
 
 %% testing mirsegment: mirsegment(... , v) -> sig.segment(... , v)
@@ -88,25 +51,14 @@ disp('<strong>testing migration: miregment(..., v) -> sig.segment(..., v)</stron
 a = miraudio(testfile);
 b = sig.signal(testfile,'Mix');
 
-v = 5;
+v = 2;
 
 a = mirsegment(a,v);
 b = sig.segment(b, v);
 
-
-tf = isequal(mirgetdata(a),b.getdata);
-
-x = mirgetdata(a);
-y = b.getdata;
+% add the test routine here
 
 
-if tf == 1
-    
-   cprintf('*green', 'test SUCCESS!\n'); 
-
-else
-   debugFail(x,y);
-end
 
 %% testing mirsegment: mirsegment(... , 'Novelty') -> sig.segment(... , 'Novelty')
 clearvars -except testfile ;
