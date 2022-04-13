01a48448608cbd0616f29c0717649cb340370083
Olivier Lartillot <olartillot@gmail.com>
2022-04-13 11:32:34 +0200

    pat.analyze rewritten

--- +pat/analyze.m
+++ +pat/analyze.m
@@ -1,148 +1,61 @@
-% Copyright (C) 2014, Olivier Lartillot
+% Copyright (C) 2022, Olivier Lartillot
 % All rights reserved.
 % License: New BSD License. See full text of the license in LICENSE.txt in
 % the main folder of the MiningSuite distribution.
-function symb = analyze(arg,metre,nbnotes)
 
-if nargin < 2
-    metre = 0;
-end
-if nargin < 3
-    nbnotes = Inf;
-end
+function analyze(inputSequence)
 
-param = seq.paramstruct;
-param.fields{1} = seq.paramtype('time');
-param.fields{1}.inter = seq.paraminter(@diff);
-param.fields{2} = seq.paramtype('rhythm');
-param.fields{3} = seq.paramtype('pitch');
-param.fields{4} = seq.paramtype('real');
+options.fuserepeat = false;
 
-root = pat.pattern([],[],param);
+% Definition of the parametrical space. Here just one dimension.
+ps = seq.paramstruct('mystruct',{'dimension'},1);
+dimension = seq.paramtype('dimension');
+ps = ps.setfield('dimension',dimension);
+
+% Creating the root of the pattern tree
+root = pat.pattern([],[],[],ps);
+
+% Creating the "occurrence" of the root. All pattern occurrences will
+% extend from that "root occurrence".
 occ0 = root.occurrence([],[]);
 
-pat.pattern(root,param);
-v = root.children{1};
-v.general = root;
-root.specific = v;
+% The sequence progressively created, starts empty
+currentSequence = seq.Sequence;
 
-pp = param;
-pp.fields{1} = param.fields{1}; %seq.paramval(param.fields{1},[]);
-pp.fields{2} = param.fields{2};
-pp.fields{3} = param.fields{3};
-pp.fields{4} = seq.paramval(param.fields{4},1);
-pat.pattern(root,pp,param);
-r = root.children{1};
-r.general = v;
-v.specific = r;
+previous = [];
+for s = inputSequence
+    % Each event successively considered in the sequence
+    s
 
-if 0 %length(arg)>4 && strcmpi(arg(end-3:end),'.txt')
-    fid = fopen(arg);
-    s = textscan(fid,'%c');
-    s = s{1};
-    s(s<'A'|s>'z') = '';
-else
-    s = arg;
-end
+    % The parametrical description of the new event
+    p = ps.type2val; % Defined from the parametrical space ps
+    % ... by associating a value to each parameter (here just one, the
+    % "dimension")
+    p = p.setfield('dimension',seq.paramval(ps.getfield('dimension'),s));
 
-p = param;
-for i = 1:size(s,2)
-    p(i).fields{1} = seq.paramval(pp.fields{1},s(1,i));
-    p(i).fields{2} = seq.paramval(pp.fields{2},[]);
-    p(i).fields{3} = seq.paramval(pp.fields{3},s(2,i));
-    p(i).fields{4} = seq.paramval(pp.fields{4},1);
-end
-           
-disp(' ');
-disp(['> Symbol #1: ',display(p(1),1)]);
+    % The event is then instantiated as a pat.event object
+    event = pat.event(currentSequence,p);
 
-new = pat.event([],p(1));
-pat.occurrence(r,[],new);
-%root.memory.learn(new.parameter,[],new,root,[]);
-occ0.memorize(new);
+    % ... integrated into the sequence under creation.
+    currentSequence = currentSequence.integrate(event);
 
-symb = new;
-for i = 2:min(length(p),nbnotes)
-    disp(' ');
-    display(['> Symbol #' num2str(i),': ',display(p(i),1)]);
+    % Now the two core mechanisms for pattern analysis:
+    if ~isempty(previous)
+        % First trying to extent any pattern occurrence ending at the
+        % previous event
+        previous.syntagm(event,root,1,options);
+        % Calling pat.event.syntagm which itself calls pat.syntagm
+        % Check pat.syntagm to see how this works.
+        % In particular, it looks at all pattern occurrence ending at
+        % previous event, and tries to extend these occurrence by calling
+        % pat.occurrence.memorize
+    end
+
+    % Then checking if the new event can be the start of a new pattern
+    % occurrence (i.e., by extending the "root occurrence" occ0)
+    occ0.memorize(event,root,[],[],1);
+    % And here also, it consists in calling pat.occurrence.memorize, where
+    % the occurrence to extend is the "root occurrence".
     
-    old = new;
-    new = pat.event([],p(i));
-    symb(i) = new;
-    pat.syntagm(old,new);
-
-    real = 0;
-    for j = 1:length(new.occurrences)
-        if ~isa(new.occurrences(j).parameter.fields{4},'seq.paramtype') && ...
-                new.occurrences(j).parameter.fields{4}.value == 1
-            real = 1;
-        end
-    end
-    if ~real
-        pat.occurrence(r,[],new);
-    end
-   
-    if ~metre
-        continue
-    end
-    
-    p0 = p(i);
-    val = Inf;
-    for j = 1:length(new.occurrences)
-        if isempty(new.occurrences(j).pattern.memory.fields{1}.inter)
-            continue
-        end
-        valj = new.occurrences(j).pattern.memory.fields{1}.inter.content.values;
-        if valj < val
-            val = valj;
-        end
-    end
-    while i == length(p) || ...
-            p(i+1).fields{1}.value - p0.fields{1}.value > val + .01
-        p0.fields{1}.value = p0.fields{1}.value + val;
-        p0.fields{2}.value = NaN;
-        p0.fields{3}.value = 0;
-        
-        disp(' ');
-        display(['> Expecting: ',display(p0,1)]);
-    
-        old = new;
-        new = pat.event(p0);
-        pat.syntagm(old,new);
-        
-        val = Inf;
-        for j = 1:length(new.occurrences)
-            if ~isempty(new.occurrences(j).pattern.children)
-                val0 = new.occurrences(j).pattern.children{1}.parameter.fields{1}.inter.value;
-                if val0 < val
-                    val = val0;
-                end
-            end
-            for k = 1:length(new.occurrences(j).pattern.specific)
-                if ~isempty(new.occurrences(j).pattern.specific(k).children)
-                    val0 = new.occurrences(j).pattern.specific(k).children{1}.parameter.fields{1}.inter.value;
-                    if val0 < val
-                        val = val0;
-                    end
-                end
-            end
-        end
-        if isinf(val)
-            for j = 1:length(new.occurrences)
-                val = new.occurrences(j).pattern.memory.fields{1}.inter.content.values;
-            end
-        end
-    end
-end
-
-disp(' ');
-disp('List of patterns:');
-root.displaytree;
-
-
-function z = diff(x,y)
-if isnumeric(x)
-    z = x - y;
-else
-    z = [];
+    previous = event;
 end
